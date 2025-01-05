145f47eba57a5ac6eb1814712b81c8ce7fbf1625
rafa-pk <phyla_cubical0j@icloud.com>
2025-01-05 07:27:45 +0000

    update headerfile

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: raica-ba <raica-ba@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 20:58:26 by raica-ba          #+#    #+#             */
-/*   Updated: 2025/01/04 21:05:28 by raica-ba         ###   ########.fr       */
+/*   Updated: 2025/01/05 07:27:25 by raica-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 # define LIBFT_H
 
 # include <stdio.h>
-# include <stdldib>
+# include <stdlib>
 # include <stdarg.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdint.h>
 
 #endif
 
