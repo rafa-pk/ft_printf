bf5ace0c2f741513f596f402acd80a08e5eefa79
rafa-pk <phyla_cubical0j@icloud.com>
2025-01-05 07:50:40 +0000

    beginning of ft_printf general structure

--- ft_printf.c
+++ ft_printf.c
@@ -6,8 +6,37 @@
 /*   By: raica-ba <raica-ba@student.42lisboa.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 17:02:19 by raica-ba          #+#    #+#             */
-/*   Updated: 2025/01/04 17:02:22 by raica-ba         ###   ########.fr       */
+/*   Updated: 2025/01/05 07:50:08 by raica-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+
+
+int	printf(char *str, ...)
+{
+	va_list	ap;
+	int		i;
+	int		print_count;
+
+	va_start(ap, str);
+	i = 0;
+	printed_count = 0;
+	while (str[i])
+	{
+		if (str[i] == '%' && str[i + 1])
+		{
+			print_count += conversion_cases();
+			i++;
+		}
+		else
+		{
+			print_count += putchar();
+			i++;
+		}
+	
+	}
+	va_end(ap);
+	return (printed_count);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,21 +6,18 @@
 /*   By: raica-ba <raica-ba@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 20:58:26 by raica-ba          #+#    #+#             */
-/*   Updated: 2025/01/05 07:27:25 by raica-ba         ###   ########.fr       */
+/*   Updated: 2025/01/05 07:35:37 by raica-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define LIBFT_H
+# define FT_LIBFT_H
 
 # include <stdio.h>
-# include <stdlib>
+# include <stdlib.h>
 # include <stdarg.h>
 # include <unistd.h>
 # include <string.h>
 # include <stdint.h>
 
 #endif
-
-
-
