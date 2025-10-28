4bac1db9d1e027fdd8f4b8c6896e27df5fc87c08
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2025-10-28 20:48:30 +0000

    Add files via upload

--- utils/ft_putnbr_base.c
+++ utils/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: rvaz-da- <rvaz-da-@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:46:59 by rvaz-da-          #+#    #+#             */
-/*   Updated: 2025/10/27 23:14:57 by rvaz-da-         ###   ########.fr       */
+/*   Updated: 2025/10/28 21:38:04 by rvaz-da-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,25 @@
 int	ft_putnbr_base(long long nb, int base, char *new_base, int flag)
 {
 	int	count;
-	int	rmd;
 
 	count = 0;
-	rmd = 0;
 	if (flag && nb == 0)
 		return (ft_putstr("(nil)"));
+	if (nb < 0 && !flag)
+	{
+		count += ft_putchar('-');
+		nb = -nb;
+	}
 	if (flag)
 	{
 		ft_putstr("0x");
 		count = 2;
 		flag = 0;
 	}
-	if (nb < 0 && !flag)
-	{
-		count += ft_putchar('-');
-		nb = -nb;
-	}
-	if (nb >= base)
-		count += ft_putnbr_base(nb / base, base, new_base, flag);
-	rmd = nb % base;
-	ft_putchar(new_base[rmd]);
+	if ((unsigned long long)nb >= (unsigned long long)base)
+		count += ft_putnbr_base((unsigned long long)nb / base,
+				base, new_base, flag);
+	ft_putchar(new_base[(unsigned long long)nb % base]);
 	count++;
 	return (count);
 }
